f90d55df9777960c5b15889d4688ef9726ef6a64
Mingming Li <mingming.li.geo@gmail.com>
2020-08-19 09:20:01 -0700

    Wed, Aug 19, 2020  9:20:01 AM

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash                                                                     
-                                                                                
-message=`date`                                                                  
-echo $message                                                                   
-git add --all .                                                                 
-git commit -m "$message"                                                        
-git push origin master
