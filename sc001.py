a65c8ba4a4d87cf91f60cd1671356f7aed397507
Mingming Li <mingming@Mingmings-MacBook-Pro.local>
2020-09-08 10:27:14 -0700

    Tue Sep  8 10:27:14 MST 2020

---
+++ sc001.py
@@ -0,0 +1,12 @@
+distance = 400. # miles 
+mpg = 30. # car mileage 
+speed = 60. #average speed 
+costPerGallon = 4.10 # price of gas
+
+time = distance / speed 
+gallons = distance / mpg 
+cost = gallons * costPerGallon
+
+print(time)
+print(gallons)
+print(cost)
