48aa78d223adc1749d9991448fca630aff0ca367
Mingming Li <mingming.li.geo@gmail.com>
2020-10-21 22:38:33 -0700

    Wed, Oct 21, 2020 10:38:33 PM

---
+++ info.py
@@ -0,0 +1,13 @@
+def sese():
+    print("SESE is great!")
+
+def asu():
+    print("ASU is great!")
+
+def ave(x):
+    return sum(x)/len(x)
+def factorial(n):
+    value=1
+    for i in range(1,n+1):
+        value=value*i
+    return value
